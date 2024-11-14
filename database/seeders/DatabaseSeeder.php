545d0a5cd90fd7d0169e10d49d30ce24a268387c
Jesus Adame <jesus.adame.sand@gmail.com>
2024-11-13 20:15:05 -0600

    feat: basic controllers

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@example.com',
+            'password' => 'secret123',
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Application;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\CustomerController;
 
 Route::redirect('/', '/login');
 
@@ -15,6 +17,10 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::resource('product', ProductController::class);
+    Route::resource('customer', controller: CustomerController::class);
+    Route::resource('order', controller: OrderController::class);
 });
 
 require __DIR__ . '/auth.php';
